4d8691f45c1c79bc9537d6760016742e637ae73b
Rakib-Coder00 <rhbappy456@gmail.com>
2023-03-25 23:19:32 +0600

    add loading effect

---
+++ src/app/loading.js
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+    return (
+        <div className="flex justify-center">
+            <img className="h-96" src="spinner.svg" alt="loading..." />
+        </div>
+    )
+}
