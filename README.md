dd619da78a32d5c35e4ab5173d8a7058deb3bf32
Rakib Hassan <rhbappy456@gmail.com>
2024-01-29 11:34:14 +0600

    Update genre filter functionality

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Open link with your browser to see the result.
 
 
-## [Imdb](https://imdb-next-js.vercel.app/) 
+## Live Demo :  [Imdb](https://imdb-next-js.vercel.app/) 
 
 <div align="center">
 <a href="https://imdb-next-js.vercel.app/">
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,23 @@
 import Results from "@/components/Results";
 
-const API = process.env.NEXT_PUBLIC_IP_API_KEY
-
+const API = process.env.NEXT_PUBLIC_IP_API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
-    `https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
     }?api_key=${API}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
-  const data = await res.json()
-  const results = data.results
+  const data = await res.json();
+  const results = data.results;
   return (
     <div>
       <Results results={results} />
     </div>
-  )
+  );
 }
